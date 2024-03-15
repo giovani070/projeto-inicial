38a15c153f0bf57b59c62b5fa56c00600f0350d8
giovani070 <bgiovani227@gmail.com>
2024-03-15 08:27:06 -0300

    Projeto Inicial

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar, false);
+var nome = document.getElementById("nome");
 
 function alertar(event){
-    alert("Você clicou no botão!!! " + event);
+    alert("Você clicou no botão!!! " + " " + nome.value);
 
     var numero = 6;
     var resultedo = numero % 2;
     if(resultado == 0){
         alert("este número é par!");
     }
-}
+}  
